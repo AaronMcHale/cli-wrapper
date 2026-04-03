c19673b11c4041024a0985fe34453afb1851692b
Aaron McHale <AaronMcHale@users.noreply.github.com>
2026-04-03 17:02:40 +0100

    Shellcheck and other fixes

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -4,10 +4,11 @@ set -u
 # Run all test_ functions in test-*.sh scripts
 
 # Cd to the directory of this script, so it can be run from anywhere
-realpath="$(realpath "$0")"
-cd "${realpath%/*}"
+realpath="$(realpath "$0")" || { echo "Failed to get realpath"; exit 1; }
+cd "${realpath%/*}" || { echo "Failed to cd to script directory"; exit 1; }
 
 # Save the current directory if tests need a absolute path reference
+# shellcheck disable=SC2034
 TEST_DIR="$PWD"
 
 # Run a test file.
@@ -16,9 +17,11 @@ TEST_DIR="$PWD"
 run_tests() {(
   local test_file="$1"
   echo -e "Running tests in $test_file...\n"
+  # shellcheck disable=SC1090
   source "$test_file"
 
   # As we're running in a subshell, we can use trap to run teardown and print message on exit.
+  # shellcheck disable=SC2329
   exit_trap() {
     # If a teardown function is defined, set it to run on EXIT
     if [[ $(declare -F teardown) ]]; then
@@ -39,8 +42,7 @@ run_tests() {(
   while read -r _ _ funcname; do
     if [[ $funcname == test_* ]]; then
       echo "Running $funcname..."
-      $funcname
-      if [[ $? -ne 0 ]]; then
+      if ! $funcname; then
         echo -e "FAIL: $funcname returned a non-zero exit code.\n"
         # We are in subshell, so can exit with 1, and trap will run teardown if defined
         exit 1
@@ -52,10 +54,9 @@ run_tests() {(
 
 TEST_FILES=(test-*.sh)
 for test_file in "${TEST_FILES[@]}"; do
-  # Only source if file exists and is not empty
+  # Confirm that test_file is a file and is not empty before trying to run it
   if [[ -f "$test_file" && -s "$test_file" ]]; then
-    run_tests "$test_file"
-    if [[ $? -ne 0 ]]; then
+    if ! run_tests "$test_file"; then
       exit 1
     fi
   fi
--- tests/test-cli.sh
+++ tests/test-cli.sh
@@ -18,7 +18,7 @@ test_cli_output_for_test_cmd() {
   cd ..
   if [[ "$(./cli.sh)" != *"test-cmd"* ]]; then
     echo "FAIL: Test command test-cmd not found in cli.sh output."
-    exit 1
+    return 1
   fi
   cd tests
 }
@@ -27,7 +27,7 @@ test_cli_from_tests_dir() {
   echo "Testing running cli.sh from tests directory, to proove that it can be run from anywhere"
   if [[ "$(./../cli.sh)" != *"test-cmd"* ]]; then
     echo "FAIL: Test command test-cmd not found when running from tests dir."
-    exit 1
+    return 1
   fi
 }
 
@@ -35,7 +35,7 @@ test_cli_symlink_from_tests_dir() {
   echo "Testing cli.sh as a symlink from tests directory, to prove that it can be run as a symlink from anywhere"
   if [[ "$(./cli-symlink.sh)" != *"test-cmd"* ]]; then
     echo "FAIL: Test command test-cmd not found when running cli.sh as a symlink from tests dir"
-    exit 1
+    return 1
   fi
 }
 
@@ -43,7 +43,7 @@ test_cli_list_output() {
   echo "Testing list sub-command to check output"
   if [[ "$(./../cli.sh list)" != *"test-cmd"* ]]; then
     echo "FAIL: Test command test-cmd not found in cli.sh list output"
-    exit 1
+    return 1
   fi
 }
 
@@ -51,7 +51,7 @@ test_list_output_for_test_cmd_help() {
   echo "Testing help text file file for test command"
   if [[ "$(./../cli.sh list)" != *"this is a test command"* ]]; then
     echo "FAIL: content not found in cli.sh list output"
-    exit 1
+    return 1
   fi
 }
 
@@ -59,7 +59,7 @@ test_help_output_for_test_cmd() {
   echo "Testing help output for test command"
   if [[ "$(./../cli.sh help test-cmd)" != *"this is a test command"* ]]; then
     echo "FAIL: content not found in cli.sh help output"
-    exit 1
+    return 1
   fi
 }
 
@@ -67,7 +67,7 @@ test_list_only_shows_first_line_of_help_file() {
   echo "Test that a second line in the help file does not show on command list"
   if [[ "$(./../cli.sh list)" = *"this line should not show on the command list"* ]]; then
     echo "FAIL: content not found in cli.sh list output"
-    exit 1
+    return 1
   fi
 }
 
@@ -75,6 +75,6 @@ test_help_shows_all_lines_of_help_file() {
   echo "Test that second line of help file shows when running help test-cmd"
   if [[ "$(./../cli.sh help test-cmd)" != *"this line should not show on the command list"* ]]; then
     echo "FAIL: content not found in cli.sh help output"
-    exit 1
+    return 1
   fi
 }
